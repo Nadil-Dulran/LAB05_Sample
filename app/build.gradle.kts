9f85477e8d180e6430f6cfe9acd6ba5f6361dc7b
Nadil-Dulran <it23161788@my.sliit.lk>
2026-03-08 23:14:57 +0530

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.lab05_sample"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.lab05_sample"
